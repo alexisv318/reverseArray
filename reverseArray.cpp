0b5fbce4f0f346ac95b84d60512cc3da7e202ab3
alexisv318 <47372306+alexisv318@users.noreply.github.com>
2019-02-05 17:38:52 -0800

    Add files via upload

---
+++ reverseArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int reverseArray() {
+
+
+}
+int main() {
+int Array[] = {1,2,3,4};
+cout << array[0] << " " << array[4] << endl;
+swap(array[0], array[1]);
+cout << array[0] << " " << array[4] << endl;
+return 0;
+
